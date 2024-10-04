17ba54a7ee7963b50d369164b6dabcadf20d0f1e
Dmitriy <sb.mobilegame@gmail.com>
2024-10-04 18:24:38 +0300

    feat: добавление типа для стиля кнопки

--- src/components/ui/buttons/AppButton.types.ts
+++ src/components/ui/buttons/AppButton.types.ts
@@ -3,6 +3,7 @@ import type { RouteLocationRaw } from '#vue-router'
 export interface AppButtonProps {
     to?: RouteLocationRaw
     title?: string
+    type?: 'solid' | 'ghost'
     color?: 'brand' | 'primary-950' | 'error-primary'
     size?: 'xs' | 's' | 'm' | 'l' | 'xl'
 }
--- src/components/ui/buttons/AppButton.vue
+++ src/components/ui/buttons/AppButton.vue
@@ -4,7 +4,8 @@ import { NuxtLink } from '#components'
 
 const props = withDefaults(defineProps<AppButtonProps>(), {
     size: 'm',
-    color: 'brand'
+    color: 'brand',
+    type: 'solid'
 })
 </script>
 
@@ -12,8 +13,8 @@ const props = withDefaults(defineProps<AppButtonProps>(), {
     <component
         :is="props.to ? NuxtLink : 'button'"
         :to="to"
-        class="app-button d-i-flex items-center clickable"
-        :class="[`app-button_size_${size}`, `app-button_color_${color}`]"
+        class="app-button d-i-flex items-center cursor-pointer"
+        :class="[`app-button_size_${size}`, `app-button_color_${color}`, `app-button_type_${type}`]"
         :title="title"
         :aria-label="title"
     >
@@ -28,21 +29,18 @@ const props = withDefaults(defineProps<AppButtonProps>(), {
     --app-button-font-size: 16px;
     --app-button-border-radius: var(--radius-m);
     --app-button-size: var(--size-form-m);
-    --app-button-bg-color: var(--color-brand-500);
-    --app-button-bg-color-hover: var(--color-brand-600);
-    --app-button-color: var(--color-primary-50);
+    --app-button-primary-color-1: var(--color-brand-500);
+    --app-button-primary-color-2: var(--color-brand-600);
+    --app-button-secondary-color-1: var(--color-primary-50);
+    --app-button-secondary-color-2: var(--color-primary-50);
 
+    transition-duration: var(--transition-default);
     font-size: var(--app-button-font-size);
-    min-height: var(--app-button-size);
-    border-radius: var(--app-button-border-radius);
-    padding-inline: calc(var(--app-button-border-radius) * 1.5);
-    color: var(--app-button-color);
-    transition: background-color var(--transition-default);
 
-    background-color: var(--app-button-bg-color);
-
-    &:hover {
-        background-color: var(--app-button-bg-color-hover);
+    &:not(&_type_ghost) {
+        padding-inline: calc(var(--app-button-border-radius) * 1.5);
+        min-height: var(--app-button-size);
+        border-radius: var(--app-button-border-radius);
     }
 
     &:disabled {
@@ -83,16 +81,45 @@ const props = withDefaults(defineProps<AppButtonProps>(), {
 
     &_color {
         &_brand {
-            --app-button-color: var(--color-primary-50);
-            --app-button-bg-color: var(--color-brand-500);
-            --app-button-bg-color-hover: var(--color-brand-600);
+            --app-button-primary-color-1: var(--color-brand-500);
+            --app-button-primary-color-2: var(--color-brand-600);
+            --app-button-secondary-color-1: var(--color-primary-50);
+            --app-button-secondary-color-2: var(--color-primary-50);
         }
 
         &_primary {
             &-950 {
-                --app-button-color: var(--color-primary-50);
-                --app-button-bg-color: var(--color-primary-950);
-                --app-button-bg-color-hover: var(--color-primary-900);
+                --app-button-primary-color-1: var(--color-primary-950);
+                --app-button-primary-color-2: var(--color-primary-900);
+                --app-button-secondary-color-1: var(--color-primary-50);
+                --app-button-secondary-color-2: var(--color-primary-50);
+            }
+        }
+    }
+
+    &_type {
+        &_ghost {
+            padding-inline: 0;
+            color: var(--app-button-primary-color-1);
+            background-color: transparent;
+            transition-property: color;
+
+            @media (hover: hover) {
+                &:hover {
+                    color: var(--app-button-primary-color-2);
+                }
+            }
+        }
+
+        &_solid {
+            color: var(--app-button-secondary-color-1);
+            background-color: var(--app-button-primary-color-1);
+            transition-property: background-color;
+
+            @media (hover: hover) {
+                &:hover {
+                    background-color: var(--app-button-primary-color-2);
+                }
             }
         }
     }
