491a8dadff8e330ba58afd09ea6682a38246829f
Dmitriy <sb.mobilegame@gmail.com>
2024-10-07 22:14:18 +0300

    feat: добавление метаданных для главной страницы

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,7 +13,14 @@ export default defineNuxtConfig({
         head: {
             htmlAttrs: {
                 class: 'scrollable'
-            }
+            },
+            link: [
+                { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+                { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+                { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+                { rel: 'manifest', href: '/site.webmanifest' },
+                { name: 'theme-color', content: '#ffffff' }
+            ]
         }
     },
     css: ['@/assets/styles/main.scss'],
@@ -43,8 +50,9 @@ export default defineNuxtConfig({
         dirs: ['stores', 'data']
     },
     routeRules: {
-        '/': { prerender: true, ssr: false },
-        '/edit': { ssr: false }
+        '/': { prerender: true },
+        '/edit': { ssr: false },
+        '/templates': { ssr: false }
     },
     vite: {
         esbuild: {
